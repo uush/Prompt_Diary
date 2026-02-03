b5aa0d7dc0da8d55c1eb53144daa24bac3803ca5
uush <dbtmd447@gmail.com>
2026-02-03 20:10:21 +0900

    feat : 버튼 활성화 조건 추가, 저장 후 Toast 추가

--- app/src/main/java/com/example/prompt_diary/ui/screens/DiaryScreen.kt
+++ app/src/main/java/com/example/prompt_diary/ui/screens/DiaryScreen.kt
@@ -1,12 +1,11 @@
 package com.example.prompt_diary.ui.screens
 
-import androidx.compose.foundation.background
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,13 +15,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.prompt_diary.ui.viewmodel.DiaryViewModel
 
 
+
 @Composable
-fun DiaryScreen(onBack: () -> Unit, viewModel: DiaryViewModel) {
+fun DiaryScreen(onBack: () -> Unit, viewModel: DiaryViewModel): Unit {
     var text by remember { mutableStateOf("") }
+    var enable by remember { mutableStateOf( false) }
+    var context = LocalContext.current
+
 
     Column (Modifier
         .fillMaxWidth()
@@ -37,7 +41,10 @@ fun DiaryScreen(onBack: () -> Unit, viewModel: DiaryViewModel) {
         // 일기 작성 박스
      OutlinedTextField(
          value = text,
-         onValueChange = {text = it},
+         onValueChange = {
+             text = it
+             enable = true
+                         },
          modifier = Modifier
              .fillMaxWidth()
              .height(400.dp)
@@ -48,10 +55,18 @@ fun DiaryScreen(onBack: () -> Unit, viewModel: DiaryViewModel) {
      )
 
         Button(
-            onClick = { viewModel.saveDiary(text)},
+            enabled = text.isNotBlank() || enable,
+            onClick = {
+                if (!enable) return@Button
+                viewModel.saveDiary(text)
+                Toast.makeText(context, "저장 완료!!", Toast.LENGTH_LONG).show()
+                text = ""
+            },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
-        ) { (Text(text="저장하기"))}
+                .padding(16.dp),
+
+        )
+        { (Text(text="저장하기"))}
     }
 }
